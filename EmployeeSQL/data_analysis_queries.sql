917c62794ee675b040d6b1566fd32f20f13a3842
kellnergp <kellnergp@gmail.com>
2021-09-17 23:58:55 -0400

    data analysis 3rd query

--- EmployeeSQL/data_analysis_queries.sql
+++ EmployeeSQL/data_analysis_queries.sql
@@ -1,6 +1,17 @@
 -- q1:  each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-JOIN salaries s
+LEFT JOIN salaries s
 ON e.emp_no = s.emp_no;
 
+-- q2: List first name, last name, and hire date for employees who were hired in 1986
+
+
+-- q3: list each manager w/ department number, department name, 
+-- the manager's employee number, last name, first name
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+LEFT JOIN departments d
+ON d.dept_no = dm.dept_no
+LEFT JOIN employees e
+ON e.emp_no = dm.emp_no;
