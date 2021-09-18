c11c3ba1977094fe0383598582a8dad3414131e5
kellnergp <kellnergp@gmail.com>
2021-09-17 23:38:48 -0400

    latter three tables created

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -25,4 +25,25 @@ CREATE TABLE departments (
 	dept_name VARCHAR(20)
 );
 
+-- create salaries table
+CREATE TABLE salaries (
+	emp_no VARCHAR(10),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary DECIMAL(9,2)
+);
 
+-- create dept employee table
+CREATE TABLE dept_emp (
+	emp_no VARCHAR(10),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- create dept manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no VARCHAR(10),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
